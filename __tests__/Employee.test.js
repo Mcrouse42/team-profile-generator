084a07ce7eb669bfa5da13c67f5916cf54e94f7f
Mcrouse <matthew.crouse@temple.edu>
2021-04-17 18:07:18 -0400

    added employee test, passed

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,22 @@
+const { TestScheduler } = require('@jest/core');
+const Employee = require('../lib/Employee');
+
+//creates employee object test 
+test('creates an employee object', () => {
+    const employee = new Employee('Jim', 1, 'jim@gmail.com');
+
+    expect(employee.name).toBe('Jim');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe('jim@gmail.com');
+
+});
+
+test('checks employee object contents', () => {
+    const employee = new Employee("Jim", 1, "jim@gmail.com");
+
+     expect(employee.getName()).toBe("Jim");
+     expect(employee.getId()).toEqual(1);
+     expect(employee.getEmail()).toBe("jim@gmail.com");
+     expect(employee.getRole()).toBe("Employee");
+});
+
