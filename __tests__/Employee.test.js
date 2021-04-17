7271860c8c25c24ac8057a3fccc239ec68f15b75
Mcrouse <matthew.crouse@temple.edu>
2021-04-17 18:16:52 -0400

    added manager test and passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,19 +3,19 @@ const Employee = require('../lib/Employee');
 
 //creates employee object test 
 test('creates an employee object', () => {
-    const employee = new Employee('Jim', 1, 'jim@gmail.com');
+    const employee = new Employee('Jim', "1", 'jim@gmail.com');
 
     expect(employee.name).toBe('Jim');
-    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.id).toBe("1");
     expect(employee.email).toBe('jim@gmail.com');
 
 });
 
 test('checks employee object contents', () => {
-    const employee = new Employee("Jim", 1, "jim@gmail.com");
+    const employee = new Employee("Jim", "1", "jim@gmail.com");
 
      expect(employee.getName()).toBe("Jim");
-     expect(employee.getId()).toEqual(1);
+     expect(employee.getId()).toBe("1");
      expect(employee.getEmail()).toBe("jim@gmail.com");
      expect(employee.getRole()).toBe("Employee");
 });
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,22 @@
+const { expect, test } = require("@jest/globals");
+const Manager = require("../lib/Manager.js");
+
+//creates manager object test
+test("creates an manager object", () => {
+  const manager = new Manager("Jim", "1", "jim@gmail.com", "9");
+
+  expect(manager.name).toBe("Jim");
+  expect(manager.id).toBe("1");
+  expect(manager.email).toBe("jim@gmail.com");
+  expect(manager.officeNumber).toBe("9");
+});
+
+test("checks employee object contents", () => {
+  const manager = new Manager("Jim", "1", "jim@gmail.com", "9");
+
+  expect(manager.getName()).toBe("Jim");
+  expect(manager.getId()).toBe("1");
+  expect(manager.getEmail()).toBe("jim@gmail.com");
+  expect(manager.getOfficeNumber()).toBe("9");
+  expect(manager.getRole()).toBe("Manager");
+});
