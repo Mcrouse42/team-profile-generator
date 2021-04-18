2fbf281149a8c3ac69a361683797e0a263ba190b
Mcrouse <matthew.crouse@temple.edu>
2021-04-18 14:28:33 -0400

    added manager and add employee prompts

--- index.js
+++ index.js
@@ -9,3 +9,57 @@ const Intern = require("./lib/Intern");
 const teamProfiles = [];
 
 //need talk to TA about how to set up
+
+function managerPrompt() {
+    inquirer.prompt([
+        {
+            type: "text",
+            name: "managerName",
+            message: "What is the name of your manager?"
+        },
+        {
+            type: "text",
+            name: "managerID",
+            message: "What is your managers ID number?"
+        },
+        {
+            type: "text",
+            name: "managerEmail",
+            message: "What is the email address of your manager?"
+        },
+        {
+            type: "text",
+            name: "managerOfficeNumber",
+            message: "What is the office number of your manager?"
+        }
+    ])
+
+}
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "addEmployee",
+            message: "Which type of employee would you like to add?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Finish building team"
+            ],
+        },
+    ])
+        .then(function (choices) {
+            switch (choices.addEmployee) {
+                case "Engineer":
+                    engineerPrompt()
+                    break;
+                case "Intern":
+                    internPrompt()
+                    break;
+                case "Finish building team":
+                    finishTeam()
+        }
+    })
+
+}
